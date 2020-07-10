63462a70267e822affccad39ff6e8c61835532dd
Danny Sepulveda <Danny.Sepulveda@ibm.com>
2020-07-10 14:10:53 -0400

    Update Card.js

--- src/components/proyectosFC/Cards/Card.js
+++ src/components/proyectosFC/Cards/Card.js
@@ -4,7 +4,6 @@ const card = (props) => {
     return (
         <div className="col-md-4">
             <div className="card mb-4 shadow-sm">
-                            { props.name }
                 <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="https://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Servicio 1">
                     <title>
                         { props.name }
